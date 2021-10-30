b313ecd7a20792b59319550e6cc89cca37f312c0
Viktor Borzov <takjchannel@gmail.com>
2021-10-30 12:50:21 +0300

    add socketService

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import TG from 'telegram-bot-api'
 import dotenv from 'dotenv'
+import SocketService from './socketService.js';
 
 dotenv.config()
 
@@ -7,20 +8,51 @@ const api = new TG({
   token: process.env.BOT_TOKEN
 })
 const mp = new TG.GetUpdateMessageProvider();
+const socketService = new SocketService()
 
+const sendUpdate = (chatId, data) => {
+  let msg = '*Обновление!* \n\n'
+  data.forEach(manga => {
+    msg += `${manga.name}\n[${manga.chapter}](${manga.link}) \n\n`
+  })
+  api.sendMessage({chat_id: chatId, text: msg, parse_mode: 'Markdown'}).catch(console.error)
+}
+
+socketService.subscribe(sendUpdate)
 api.setMessageProvider(mp);
-api.start().then(() => console.log('ready'));
+api.start().then(() => console.log('Manga Observer bot is up'));
 api.setMyCommands({commands: [
   {
     command: '/info',
     description: 'Информация о боте'
   },
   {
-    command: '/new_manga',
-    description: 'Информация о новых главах манги'
+    command: '/subscribe',
+    description: 'Подписка на выход новых глав манги. Бот будет присылать сообщение, когда выйдет новая глава'
   },
-]}).then(() => api.getMyCommands().then(console.log)).catch(console.error)
+]}).catch(console.error)
 
 api.on('update', ({message}) => {
-  api.sendMessage({chat_id: chatId, text: message.text}).catch(console.error)
+  const chatId = message.chat.id
+  const rawMessage = message.text.split(' ')
+  const command = rawMessage[0]
+
+  if (chatId.toString() !== process.env.MY_ID) {
+    api.sendMessage({chat_id: chatId, text: 'Добро пожаловать!\nЭто персональный бот для уведомления о выходе новых глав манги\nВы не можете им пользоваться'}).catch(console.error)
+    return
+  }
+
+  switch(command) {
+    case '/info': 
+      api.sendMessage({chat_id: chatId, text: 'Бот для уведомления о выходе новых глав манги'}).catch(console.error)
+      break
+    case '/subscribe':
+      api.sendMessage({chat_id: chatId, text: 'Вы подписались на рассылку уведомлений о выходе новых глав манги'}).catch(console.error)
+      socketService.addSubscriber(chatId)
+      break
+    default:
+      api.sendMessage({chat_id: chatId, text: 'Введите команду'}).catch(console.error)
+      break
+  }
 })
+
---
+++ src/socketService.js
@@ -0,0 +1,22 @@
+import io from 'socket.io-client'
+
+export default class SocketService {
+  constructor() {
+    this.subscribers = []
+    this.io = io(process.env.MANGA_SERVER)
+  }
+
+  addSubscriber(id) {
+    const subscriberExists = this.subscribers.some(user => user === id)
+
+    if (subscriberExists) return;
+
+    this.subscribers.push(id)
+  }
+
+  subscribe(cb) {
+    this.io.on('manga', (data) => {
+      this.subscribers.forEach(user => cb(user, data))
+    })
+  }
+}
